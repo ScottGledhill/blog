42a141a2bf754b591ce2e60ff44ec2af250175cc
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-12-16 14:44:31 +0000

    update actipom added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,11 +13,21 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    
+
     if @article.save
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
