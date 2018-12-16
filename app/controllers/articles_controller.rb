705404836617e52a9ae667b98e54b1add1029641
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-12-16 14:53:55 +0000

    destroy action added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    
+
     if @article.update(article_params)
       redirect_to @article
     else
@@ -34,6 +34,14 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+ 
+  redirect_to articles_path
+end
 
 private
   def article_params
